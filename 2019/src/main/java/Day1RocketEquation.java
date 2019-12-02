4866475a585fd0a1bbea42650403cd025d200fde
Joshua Ramirez <josh20615@gmail.com>
2019-12-01 20:54:00 -0800

    part 1 and 2 fuctions along with test cases based on provided examples. Data file for submission

---
+++ 2019/src/main/java/Day1RocketEquation.java
@@ -0,0 +1,68 @@
+import java.util.List;
+
+public class Day1RocketEquation {
+    /**
+     * Part 1 function, which gathers the initial fuel required based on the masses provided.
+     * @param masses all the masses to be used for the calculation of required fuel
+     * @return weight in fuel required
+     */
+    public static Integer getRequiredInitialFuel(List<String> masses) {
+        Integer sum = 0;
+        for(String mass: masses) {
+            Integer massAsInt = Integer.parseInt(mass);
+            sum += getFuel(massAsInt);
+        }
+
+        return sum;
+    }
+
+    // TODO(JOSH): the above and below functions may be able to be made into a single function? The only difference is
+    //  the function call.
+    /**
+     * Part 2 function, which now needs to include the fuel weight recursively until each module requires 0 new fuel
+     * @param masses all the masses to be used for the calculation of required fuel
+     * @return weight in fuel required
+     */
+    public static Integer getRequiredTotalFuel(List<String> masses) {
+        Integer sum = 0;
+        for(String mass: masses) {
+            Integer massAsInt = Integer.parseInt(mass);
+            sum += getRequiredExtraFuel(massAsInt,0);
+        }
+
+        return sum;
+    }
+
+    /**
+     * Recursively gets the fuel required for a module until the fuel for the fuel is less than or equal to 0. Based on
+     * the following constraint:
+     * 'So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just
+     * calculated as the input mass and repeat the process, continuing until a fuel requirement is zero or negative.'
+     * @param newMass mass being passed in to evaluate the fuel needed
+     * @param fuel accumulated fuel
+     * @return the total fuel weight requirement for the module
+     */
+    public static Integer getRequiredExtraFuel(Integer newMass, Integer fuel) {
+        Integer mass = getFuel(newMass);
+        if (mass <= 0) {
+            return fuel;
+        }
+
+        fuel += mass;
+        return getRequiredExtraFuel(mass, fuel);
+    }
+
+    /**
+     * Gets the fuel required for the mass passed in based on the following constraint:
+     * 'Fuel required to launch a given module is based on its mass. Specifically, to find the fuel required for a module,
+     * take its mass, divide by three, round down, and subtract 2.'
+     *
+     * @param mass mass of the module which will be used to get the weight
+     * @return the fuel weight required for the given mass
+     */
+    private static Integer getFuel(Integer mass) {
+        Integer divideMassBy3 = mass / 3;
+
+        return divideMassBy3 - 2;
+    }
+}
---
+++ 2019/src/test/java/Day1RocketEquationTest.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day1RocketEquationTest {
+
+    @Test
+    public void test12() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("12");
+        assertEquals(2, Day1RocketEquation.getRequiredInitialFuel(mockFileResults),0);
+    }
+
+    @Test
+    public void test14() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("14");
+        assertEquals(2, Day1RocketEquation.getRequiredInitialFuel(mockFileResults), 0);
+    }
+
+    @Test
+    public void test1969() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("1969");
+        assertEquals(654, Day1RocketEquation.getRequiredInitialFuel(mockFileResults),0);
+    }
+
+    @Test
+    public void test100756() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("100756");
+        assertEquals(33583, Day1RocketEquation.getRequiredInitialFuel(mockFileResults),0);
+    }
+
+    @Test
+    public void testList() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("12");
+        mockFileResults.add("14");
+        mockFileResults.add("1969");
+        mockFileResults.add("100756");
+        assertEquals((2+2+654+33583), Day1RocketEquation.getRequiredInitialFuel(mockFileResults), 0);
+    }
+
+    @Test
+    public void testPart1() {
+        List<String> fileResults = AocFileReader.readFile("src/main/resources/Day1Data.txt");
+        assertEquals(3303995, Day1RocketEquation.getRequiredInitialFuel(fileResults),0);
+    }
+
+    @Test
+    public void test14part2() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("14");
+        assertEquals(2, Day1RocketEquation.getRequiredTotalFuel(mockFileResults), 0);
+    }
+
+    @Test
+    public void test1969part2() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("1969");
+        assertEquals(966, Day1RocketEquation.getRequiredTotalFuel(mockFileResults), 0);
+    }
+
+    @Test
+    public void test100756part2() {
+        List<String> mockFileResults = new ArrayList();
+        mockFileResults.add("100756");
+        assertEquals(50346, Day1RocketEquation.getRequiredTotalFuel(mockFileResults), 0);
+    }
+
+    //4955948 too high
+    @Test
+    public void testPart2() {
+        List<String> fileResults = AocFileReader.readFile("src/main/resources/Day1Data.txt");
+        assertEquals(4953118, Day1RocketEquation.getRequiredTotalFuel(fileResults), 0);
+    }
+}
