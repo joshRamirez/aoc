321f37f7d9e2cb079c3af2280a18bc23a179b995
Joshua Ramirez <josh20615@gmail.com>
2019-12-09 22:38:08 -0800

    part 2 solution and tests

--- 2019/src/main/java/Day3CrossedWires.java
+++ 2019/src/main/java/Day3CrossedWires.java
@@ -1,15 +1,13 @@
 import java.awt.*;
-import java.util.HashSet;
+import java.util.*;
 import java.util.List;
-import java.util.Set;
 
 public class Day3CrossedWires {
-    public static int getClosestCross(List<List<String>> paths) {
+    public static int getClosestIntersection(List<List<String>> paths) {
         List<String> wire0 = paths.get(0);
         List<String> wire1 = paths.get(1);
 
-        Set positions = new HashSet();
-        positions = mapOutPath(wire0);
+        Set positions = mapOutPath(wire0);
         return findSmallestCollision(positions, wire1);
     }
 
@@ -69,6 +67,81 @@ public class Day3CrossedWires {
 
 
                 positions.add(new Point(x, y));
+            }
+        }
+
+        return positions;
+    }
+
+    public static int getShortestDistanceIntersection(List<List<String>> paths) {
+        List<String> wire0 = paths.get(0);
+        List<String> wire1 = paths.get(1);
+
+        Map positions = mapOutPathShortestDistance(wire0);
+        return findShortestIntersection(positions, wire1);
+    }
+
+    private static int findShortestIntersection(Map<Point, Integer> positions, List<String> wire1) {
+        Integer minCollision = null;
+        int x = 0;
+        int y = 0;
+        int steps = 0;
+
+        for (String wirePosition : wire1) {
+            String direction = wirePosition.substring(0, 1);
+            Integer iterations = Integer.parseInt(wirePosition.substring(1));
+
+            for (int i = 0; i < iterations; i++) {
+                steps++;
+                if (direction.equals("U")) {
+                    y++;
+                } else if (direction.equals("L")) {
+                    x--;
+                } else if (direction.equals("R")) {
+                    x++;
+                } else {
+                    y--;
+                }
+
+                if (positions.containsKey(new Point(x, y))) {
+                    int manhattanDistance = positions.get(new Point(x, y)) + steps;
+                    if (null != minCollision) {
+                        minCollision = Math.min(minCollision, manhattanDistance);
+                    } else {
+                        minCollision = manhattanDistance;
+                    }
+                }
+            }
+        }
+
+        return minCollision;
+    }
+
+    private static Map mapOutPathShortestDistance(List<String> wire0) {
+        Map positions = new HashMap();
+        int x = 0;
+        int y = 0;
+        int steps = 0;
+
+        for (String wirePosition : wire0) {
+            String direction = wirePosition.substring(0, 1);
+            Integer iterations = Integer.parseInt(wirePosition.substring(1));
+
+            for (int i = 0; i < iterations; i++) {
+                steps ++;
+                if (direction.equals("U")) {
+                    y++;
+                } else if (direction.equals("L")) {
+                    x--;
+                } else if (direction.equals("R")) {
+                    x++;
+                } else {
+                    y--;
+                }
+
+                if (!positions.containsKey(new Point(x, y))) {
+                    positions.put(new Point(x, y), steps);
+                }
             }
         }
 
--- 2019/src/test/java/Day3CrossedWiresTest.java
+++ 2019/src/test/java/Day3CrossedWiresTest.java
@@ -14,7 +14,7 @@ public class Day3CrossedWiresTest {
         List<String> mockList1 = Arrays.asList("U62,R66,U55,R34,D71,R55,D58,R83".split(","));
         mockLists.add(mockList0);
         mockLists.add(mockList1);
-        assertEquals(159, Day3CrossedWires.getClosestCross(mockLists));
+        assertEquals(159, Day3CrossedWires.getClosestIntersection(mockLists));
     }
 
     @Test
@@ -24,12 +24,38 @@ public class Day3CrossedWiresTest {
         List<String> mockPath1 = Arrays.asList("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(","));
         mockPaths.add(mockPath0);
         mockPaths.add(mockPath1);
-        assertEquals(135, Day3CrossedWires.getClosestCross(mockPaths));
+        assertEquals(135, Day3CrossedWires.getClosestIntersection(mockPaths));
     }
 
     @Test
     public void testClosestWiresPart1() {
         List<List<String>> paths = AocFileReader.readList("src/main/resources/Day3Data.txt");
-        assertEquals(709, Day3CrossedWires.getClosestCross(paths));
+        assertEquals(709, Day3CrossedWires.getClosestIntersection(paths));
+    }
+
+    @Test
+    public void testGetShortestWires() {
+        List<List<String>> mockLists = new ArrayList();
+        List<String> mockList0 = Arrays.asList("R75,D30,R83,U83,L12,D49,R71,U7,L72".split(","));
+        List<String> mockList1 = Arrays.asList("U62,R66,U55,R34,D71,R55,D58,R83".split(","));
+        mockLists.add(mockList0);
+        mockLists.add(mockList1);
+        assertEquals(610, Day3CrossedWires.getShortestDistanceIntersection(mockLists));
+    }
+
+    @Test
+    public void testGetShortestWires2() {
+        List<List<String>> mockPaths = new ArrayList();
+        List<String> mockPath0 = Arrays.asList("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(","));
+        List<String> mockPath1 = Arrays.asList("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(","));
+        mockPaths.add(mockPath0);
+        mockPaths.add(mockPath1);
+        assertEquals(410, Day3CrossedWires.getShortestDistanceIntersection(mockPaths));
+    }
+
+    @Test
+    public void testShortestWiresPart2() {
+        List<List<String>> paths = AocFileReader.readList("src/main/resources/Day3Data.txt");
+        assertEquals(13836, Day3CrossedWires.getShortestDistanceIntersection(paths));
     }
 }
